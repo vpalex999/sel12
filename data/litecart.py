e0c4e39c27510e60542779d9b182120233a9092b
vpalex999 <vpalex@mail.ru>
2017-11-09 08:28:53 +0500

    fixes url

--- data/litecart.py
+++ data/litecart.py
@@ -3,7 +3,7 @@
 """
 
 
-url = 'http://192.168.10.168:81'
+url = 'http://192.168.10.168:81/litecart/'
 url_admin = 'http://192.168.10.168:81/litecart/admin'
 
 path_to_nightly = "/home/vpalexdev/Downloads/firefox-nightly/firefox/firefox"
